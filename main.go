618dca84ac1a3ee0bcb2dbdd972d2c94f4c758a5
erickuce1509 <erickuce1509@gmail.com>
2025-02-12 20:09:19 -0500

    Test 7 commit to trigger GitHub Actions workflow

--- main.go
+++ main.go
@@ -57,6 +57,6 @@ func main() {
 	}
 
 	// ✅ Iniciar el servidor en el puerto definido en `.env`
-	fmt.Println("✅ Servidor uwunt corriendo en el puerto", port)
+	fmt.Println("✅ Servidor uwunts corriendo en el puerto", port)
 	log.Fatal(http.ListenAndServe(":"+port, handler)) // ✅ Ahora sí usa CORS
 }
